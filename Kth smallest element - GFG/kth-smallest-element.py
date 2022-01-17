c8ca1e61ba0df6907a02e8e5df56375b8f46bd5d
Ankit Kumar <ankitkr0696@gmail.com>
2022-01-17 11:00:56 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.py
+++ Kth smallest element - GFG/kth-smallest-element.py
@@ -1,4 +1,21 @@
 #User function Template for python3
+import random
+def partition(A,left,right,test):
+    p=random.randint(left,right)
+    pivot=A[p]
+    A[p],A[right]=A[right],A[p]
+    j=left
+    for i in range(left,right):
+        if A[i]<=pivot:
+            A[i],A[j]=A[j],A[i]
+            j+=1
+    A[j],A[right]=A[right],A[j]
+    if(j==test):
+        return A[test]
+    elif(j<test):
+        return partition(A,j+1,right,test)
+    else:
+        return partition(A,left,j-1,test)
 
 class Solution:
     def kthSmallest(self,arr, l, r, k):
@@ -8,8 +25,10 @@ class Solution:
         r : ending index of the array i.e size-1
         k : find kth smallest element and return using this function
         '''
-        arr.sort()
-        return arr[k-1]
+        
+        return partition(arr,l,r,k-1)
+
+    
             
 
 #{ 
